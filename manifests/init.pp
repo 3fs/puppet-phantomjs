463e424612510a806e7b29b6e6f09989c0ea7a4f
Martin Zibert <martin.zibert@3fs.si>
2013-11-11 19:31:21 +0100

    add $source_url in case you need to fetch a package from different url.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,13 @@
 class phantomjs (
   $package_version = '1.9.2', # set package version to download
+  $source_url = "https://phantomjs.googlecode.com/files/phantomjs-$package_version-linux-x86_64.tar.bz2"
   $source_dir = '/opt',
   $install_dir = '/usr/local/bin',
   $package_update = false,
 
 ) {
   exec { 'get phantomjs':
-    command => "/usr/bin/curl --silent --show-error https://phantomjs.googlecode.com/files/phantomjs-$package_version-linux-x86_64.tar.bz2 --output $source_dir/phantomjs.tar.bz2 && mkdir $source_dir/phantomjs && tar xvf $source_dir/phantomjs.tar.bz2 -C $source_dir && mv $source_dir/phantomjs-$package_version-linux-x86_64/* $source_dir/phantomjs/ && rm -rf $source_dir/phantomjs-$package_version-linux-x86_64",
+    command => "/usr/bin/curl --silent --show-error $source_url --output $source_dir/phantomjs.tar.bz2 && mkdir $source_dir/phantomjs && tar xvf $source_dir/phantomjs.tar.bz2 -C $source_dir && mv $source_dir/phantomjs-$package_version-linux-x86_64/* $source_dir/phantomjs/ && rm -rf $source_dir/phantomjs-$package_version-linux-x86_64",
     creates => "$source_dir/phantomjs/",
     require => Package['curl', 'unzip'],
   }
