9ee24f64ee10c8b8a9deb20df3633c8af27dd22d
Igor Ajdisek <igor.ajdisek@gmail.com>
2014-05-06 16:31:43 +0200

    Made syntax compliant with puppet-lint.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,17 @@
+# Base class for phatomjs module
 class phantomjs (
-  $package_version = '1.9.2', # set package version to download
-  $source_url = "https://phantomjs.googlecode.com/files/phantomjs-$package_version-linux-x86_64.tar.bz2",
+  $package_version = '1.9.2',
+  $source_url = 'UNSET',
   $source_dir = '/opt',
   $install_dir = '/usr/local/bin',
   $package_update = false,
 
 ) {
+
+  $pkg_src_url = $source_url ? {
+    'UNSET' => "https://phantomjs.googlecode.com/files/phantomjs-${package_version}-linux-x86_64.tar.bz2",
+    default => $source_url,
+  }
 
   if ! defined(Package['curl']) {
     package { 'curl':
@@ -26,24 +32,24 @@ class phantomjs (
   }
 
   exec { 'get phantomjs':
-    command => "/usr/bin/curl --silent --show-error $source_url --output $source_dir/phantomjs.tar.bz2 \
-      && mkdir $source_dir/phantomjs && tar xjf $source_dir/phantomjs.tar.bz2 -C $source_dir \
-      && mv $source_dir/phantomjs-$package_version-linux-x86_64/* $source_dir/phantomjs/ \
-      && rm -rf $source_dir/phantomjs-$package_version-linux-x86_64",
-    creates => "$source_dir/phantomjs/",
+    command => "/usr/bin/curl --silent --show-error ${pkg_src_url} --output ${source_dir}/phantomjs.tar.bz2 \
+      && mkdir ${source_dir}/phantomjs && tar xjf ${source_dir}/phantomjs.tar.bz2 -C ${source_dir} \
+      && mv ${source_dir}/phantomjs-${package_version}-linux-x86_64/* ${source_dir}/phantomjs/ \
+      && rm -rf ${source_dir}/phantomjs-${package_version}-linux-x86_64",
+    creates => "${source_dir}/phantomjs/",
     require => Package['curl', 'bzip2'],
   }
 
-  file { "$install_dir/phantomjs":
+  file { "${install_dir}/phantomjs":
     ensure => link,
-    target => "$source_dir/phantomjs/bin/phantomjs",
-    force => true,
+    target => "${source_dir}/phantomjs/bin/phantomjs",
+    force  => true,
   }
 
   if $package_update {
     exec { 'remove phantomjs':
-      command => "/bin/rm -rf $source_dir/phantomjs",
-      notify => Exec[ 'get phantomjs' ]
+      command => "/bin/rm -rf ${source_dir}/phantomjs",
+      notify  => Exec[ 'get phantomjs' ]
     }
   }
 }
